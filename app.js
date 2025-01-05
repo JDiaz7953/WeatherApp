5a330019f1bd16c113f21297983cc762e034cfb1
Jaiden <jaidendiaz7953@gmail.com>
2025-01-05 12:44:32 -0500

    Search Bar added with submit button

---
+++ app.js
@@ -0,0 +1,16 @@
+const action = document.querySelector('button')
+const request = document.querySelector('input')
+
+let findLocation = (location) => {
+    //this function is going to be called inside the event-listener 
+
+    //TODO
+    /*
+    This function is going to be the function that grabs the location the is in the input,
+    then update the webpage with the location and forcast of the location that was selected
+    */
+}
+
+action.addEventListener('click', () => {
+    console.log(request.value)
+})
